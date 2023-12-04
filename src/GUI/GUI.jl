356b4d15ce9d260a3eb2bd48a1a7bd40768229fc
Daniel Bulanda <daniel@bulanda.net>
2023-12-04 12:51:50 +0100

    Add button widgets

--- src/GUI/GUI.jl
+++ src/GUI/GUI.jl
@@ -11,94 +11,98 @@ using FlexPoints
 
 include("widgets.jl")
 
+mutable struct TopBarState
+    layout::GridLayout
+    state::Observable{Vector{Bool}}
+end
+
 struct UIState
     figure::Figure
-    topbar::GridLayout
+    topbar::TopBarState
     leftpanel::GridLayout
     centralpanel::GridLayout
     rightpanel::GridLayout
     bottombar::GridLayout
+    nightmode::Observable{Bool}
+    styles::Styles
 end
 
 function gui(;
     resolution=primary_resolution(), darkmode=true
 )
     uistate = prepareui(resolution, darkmode)
-  
-    scene, lines, points = drawgraph(uistate.centralpanel)
 
-    drawmenu(uistate.topbar)
+    scene, lines, points = drawgraph(uistate.centralpanel, resolution)
 
-    # connect(scene, darkmode, colorpicker, lines, points)
+    drawmenu(uistate)
 
     uistate.figure
 end
 
-function connect(scene, darkmode, colorpicker, lines, points)
-    on(c -> points.color = c, colorpicker.selection)
-    notify(colorpicker.selection)
+function drawmenu(uistate::UIState)
+    figure = uistate.figure
+    topbar = uistate.topbar
+    layout = topbar.layout
+    nightmode = uistate.nightmode
+    styles = uistate.styles
 
-    on(mode -> applymode(scene, mode), darkmode.active)
-    notify(darkmode.active)
+    daynight(layout[1, 1], nightmode, figure, styles)
+    tagexclusive(layout[1, 2], "▤ dataload", topbar.state, 1)
+    tagexclusive(layout[1, 3], "⛁ data", topbar.state, 2)
+    tagexclusive(layout[1, 4], "❉ data", topbar.state, 3)
+    tagexclusive(layout[1, 5], " data", topbar.state, 4)
+    tag(layout[1, 6], "䷀ 3D ䷀", Observable(true))
+    Box(layout[1, 7], color=RGBAf(0, 0, 0, 0), height=BUTTON_HEIGHT, strokevisible=false)
 end
 
-function drawmenu(topbar)
-    # darkmode = Toggle(figure, active=true)
-    # darkmode_label = Label(figure, lift(x -> x ? "dark mode" : "light mode", darkmode.active))
-
-    # colorpicker = Menu(figure, options = ["blue", "green", "white"], default = "white")
-    
-    tag(topbar[1, 1], "▤|dataload")
-    tag(topbar[1, 2], "⛁|data")
-    tag(topbar[1, 3], "❉|data")
-    tag(topbar[1, 4], "|data")
-    tag(topbar[1, 5], "䷀ 3D ䷀")
-    Box(topbar[1, 6], color=RGBAf(0, 0, 0, 0), height=BUTTON_HEIGHT, strokevisible=false)
-    # figure[1, 1] = hgrid!(
-    #     Button(figure, label="Data", height=BUTTON_HEIGHT),
-    #     # darkmode_label, darkmode,
-    #     # Label(figure, "Colormap", width = nothing), colorpicker,
-    #     Box(figure, color=RGBAf(0, 0, 0, 0)),
-    #     tellheight=true, height=TOP_BAR_HEIGHT
+function drawgraph(figure, resolution)
+    # scene = LScene(
+    #     figure[2, 1],
+    #     show_axis=true,
+    #     # height=resolution[2] / 2,
+    #     # width=resolution[1] / 2,
+    #     scenekw=(
+    #         viewport=Rect(0, 0, resolution[1], resolution[2]),
+    #         clear=false,
+    #         backgroundcolor=:black,
+    #     )
     # )
+    # cam2d!(scene.scene)
 
-    # colorpicker
-end
-
-function drawgraph(figure)
-    scene = LScene(
-        figure[2, 1],
-        show_axis=true,
-        scenekw = (
-            clear=true,
-            backgroundcolor=:black
-        )
+    # size(figure[2, 1])
+    axis = Axis(
+        figure[1, 1],
+        ylabel="signal [mV]",
+        xlabel="time [ms]",
+        # title="ECG",
+        xrectzoom=false,
+        yrectzoom=false,
+        valign=:center,
+        limits=(0, 50, -2, 2),
+        xlabelfont=:juliamono_light,
+        xticklabelfont=:juliamono_light,
+        ylabelfont=:juliamono_light,
+        yticklabelfont=:juliamono_light,
     )
+    hidespines!(axis)
+    # hidespines!(axis, :t, :r)
 
-    cam2d!(scene.scene)
+    xs = LinRange(0, 100, 1000)
+    ys = 1.5 .* sin.(xs)
 
-    xs = LinRange(0, 10, 130)
-    ys = 0.5 .* sin.(xs)
-
-    lines = scatterlines!(scene, xs, ys, color=:green, markersize=0)
+    lines = scatterlines!(axis, xs, ys, color=:green, markersize=0)
 
     data = collect(zip(xs, ys))
     indices = flexpoints(data, (true, true, true, true))
     points = [data[i...] for i in indices]
 
-    points = scatter!(scene, points, color=:white, markersize=8)
+    points = scatter!(axis, points, color=:white, markersize=8)
 
-    scene, lines, points
+    axis, lines, points
 end
 
-function prepareui(resolution::Tuple{Integer, Integer}, darkmode::Bool)::UIState
-    if darkmode
-        set_theme!(theme_black(), resolution=resolution)
-    else
-        set_theme!(theme_light(), resolution=resolution)
-    end
-
-    GLMakie.activate!(; 
+function prepareui(resolution::Tuple{Integer,Integer}, darkmode::Bool)::UIState
+    GLMakie.activate!(;
         fullscreen=true,
         framerate=120,
         fxaa=true,
@@ -106,33 +110,45 @@ function prepareui(resolution::Tuple{Integer, Integer}, darkmode::Bool)::UIState
         vsync=true
     )
 
-    
+    if darkmode
+        set_theme!(theme_black(), size=resolution)
+    else
+        set_theme!(theme_light(), size=resolution)
+    end
+
     loadfonts()
 
-    figure = Figure(figure_padding=5)
+    figure = Figure(figure_padding=0)
 
-    topbar = figure[1, 1:3] = GridLayout(alignmode=Outside(LAYOUT_PADDING))
+    topbarlayout = figure[1, 1:3] = GridLayout(alignmode=Outside(LAYOUT_PADDING))
+    topbar = TopBarState(
+        topbarlayout, Observable([false, true, false, false, false]))
+
     leftpanel = figure[2, 1] = GridLayout()
     centralpanel = figure[2, 2] = GridLayout()
     rightpanel = figure[2, 3] = GridLayout()
     bottombar = figure[3, 1:3] = GridLayout()
-    
-    UIState(
+
+    colsize!(figure.layout, 1, Auto(15.0))
+    colsize!(figure.layout, 2, Auto(100.0))
+    colsize!(figure.layout, 3, Auto(15.0))
+
+    rowsize!(figure.layout, 1, Fixed(TOP_BAR_HEIGHT))
+    rowsize!(figure.layout, 2, Auto(1.0))
+    rowsize!(figure.layout, 3, Fixed(TOP_BAR_HEIGHT))
+
+    state = UIState(
         figure,
         topbar,
         leftpanel,
         centralpanel,
         rightpanel,
         bottombar,
+        Observable(true),
+        Styles()
     )
-end
 
-function applymode(scene, darkmode::Bool)
-    if darkmode
-        scene.scene.backgroundcolor = parse(RGBA, theme_black().backgroundcolor[])
-    else
-        scene.scene.backgroundcolor = colorant"white"
-    end
+    state
 end
 
 function primary_resolution()
@@ -142,7 +158,7 @@ function primary_resolution()
 end
 
 function loadfonts()
-    fonts = Dict{Symbol, FTFont}()
+    fonts = Dict{Symbol,FTFont}()
     fontspath = joinpath("asset", "font")
     for fontfile in readdir(fontspath)
         fontpath = joinpath(fontspath, fontfile)
@@ -152,7 +168,7 @@ function loadfonts()
             fonts[fontid] = font
         end
     end
-    
+
 
     update_theme!(
         fonts=NamedTuple{Tuple(keys(fonts))}(values(fonts))
--- src/GUI/widgets.jl
+++ src/GUI/widgets.jl
@@ -1,22 +1,102 @@
 using GLMakie
+using Colors
+using Match
 
-include("consts.jl")
+include("styles.jl")
 
-function tag(
-    target, label;
-    height=BUTTON_HEIGHT
+function tagexclusive(
+    target, label, state::Observable{Vector{Bool}}, index::Integer;
 )
-    Button(
-        target; 
+    buttoncolor = lift(x -> x[index] ? :ivory3 : :gray7, state)
+    buttoncolor_hover = lift(x -> x[index] ? :ivory2 : :gray14, state)
+    buttoncolor_active = lift(x -> x[index] ? :ivory1 : :gray21, state)
+    labelcolor = lift(x -> x[index] ? :gray8 : :gray95, state)
+
+    button = Button(
+        target;
         label=label,
         tellheight=true,
-        padding=[12, 0, 5, 0],
+        padding=[12, 2, 2, 2],
         strokewidth=1,
-        buttoncolor=:deepskyblue4,
-        strokecolor=:deepskyblue4,
+        buttoncolor=buttoncolor,
+        buttoncolor_hover=buttoncolor_hover,
+        buttoncolor_active=buttoncolor_active,
+        strokecolor=:transparent,
         font=:juliamono_light,
-        fontsize=22,
-        labelcolor=:gray98,
-        cornerradius=7,
+        fontsize=13.8,
+        labelcolor=labelcolor,
+        labelcolor_active=labelcolor,
+        labelcolor_hover=labelcolor,
+        cornerradius=5,
     )
+
+    on(button.clicks) do _c
+        state[][index] = !state[][index]
+        if state[][index]
+            for i in 1:length(state[])
+                if i != index
+                    state[][i] = false
+                end
+            end
+        end
+        notify(state)
+    end
+end
+
+function tag(
+    target, label, state::Observable{Bool};
+)::Button
+    buttoncolor = lift(x -> x ? :ivory3 : :gray7, state)
+    buttoncolor_hover = lift(x -> x ? :ivory2 : :gray14, state)
+    buttoncolor_active = lift(x -> x ? :ivory1 : :gray21, state)
+    labelcolor = lift(x -> x ? :gray8 : :gray95, state)
+
+    button = Button(
+        target;
+        label=label,
+        tellheight=true,
+        padding=[12, 2, 2, 2],
+        strokewidth=1,
+        buttoncolor=buttoncolor,
+        buttoncolor_hover=buttoncolor_hover,
+        buttoncolor_active=buttoncolor_active,
+        strokecolor=:transparent,
+        font=:juliamono_light,
+        fontsize=13.8,
+        labelcolor=labelcolor,
+        labelcolor_hover=labelcolor,
+        labelcolor_active=labelcolor,
+        cornerradius=5,
+    )
+
+    on(button.clicks) do _c
+        state[] = !state[]
+        notify(state)
+    end
+
+    button
+end
+
+function daynight(target, nightmode::Observable{Bool}, figure::Figure, styles::Styles)::Button
+    label = lift(nightmode) do state
+        @match state begin
+            false => "☼ light mode"
+            true => "☽ night mode"
+        end
+    end
+    button = tag(target, label, nightmode)
+
+    applystyle(figure, nightmode, styles)
+
+    button
+end
+
+function applystyle(figure::Figure, nightmode::Observable{Bool}, styles::Styles)
+    on(nightmode) do nightmode
+        if nightmode
+            figure.scene.backgroundcolor[] = styles.darkmode.background
+        else
+            figure.scene.backgroundcolor[] = styles.lightmode.background
+        end
+    end
 end
